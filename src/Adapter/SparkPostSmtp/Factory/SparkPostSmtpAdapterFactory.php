f52c8b0308932a2b0abc4091a98c12e802f4a948
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-10-20 15:44:16 +0200

    Added method to send message.

--- src/Adapter/SparkPostSmtp/Factory/SparkPostSmtpAdapterFactory.php
+++ src/Adapter/SparkPostSmtp/Factory/SparkPostSmtpAdapterFactory.php
@@ -2,7 +2,41 @@
 
 namespace MassimoFilippi\MailModule\Adapter\SparkPostSmtp\Factory;
 
-class SparkPostSmtpAdapterFactory
+use Interop\Container\ContainerInterface;
+use MassimoFilippi\MailModule\Adapter\SparkPostSmtp\SparkPostSmtpAdapter;
+use Zend\ServiceManager\Exception\ServiceNotCreatedException;
+use Zend\ServiceManager\Factory\FactoryInterface;
+
+/**
+ * Class SparkPostSmtpAdapterFactory
+ * @package MassimoFilippi\MailModule\Adapter\SparkPostSmtp\Factory
+ */
+class SparkPostSmtpAdapterFactory implements FactoryInterface
 {
 
+    /**
+     * @param ContainerInterface $container
+     * @param string $requestedName
+     * @param array|null $options
+     * @return SparkPostSmtpAdapter
+     */
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
+    {
+
+        /** @var array $config */
+        $config = $container->get('Config');
+
+        if (false === isset($config['massimo_filippi']['mail_module']['adapter_params']['name'])) {
+            throw new ServiceNotCreatedException('Missing adapter parameter: "name".');
+        }
+
+        if (false === isset($config['massimo_filippi']['mail_module']['adapter_params']['password'])) {
+            throw new ServiceNotCreatedException('Missing adapter parameter: "password".');
+        }
+
+        $name = $config['massimo_filippi']['mail_module']['adapter_params']['name'];
+        $password = $config['massimo_filippi']['mail_module']['adapter_params']['password'];
+
+        return new SparkPostSmtpAdapter($name, $password);
+    }
 }
--- src/Adapter/SparkPostSmtp/SparkPostSmtpAdapter.php
+++ src/Adapter/SparkPostSmtp/SparkPostSmtpAdapter.php
@@ -4,6 +4,10 @@ namespace MassimoFilippi\MailModule\Adapter\SparkPostSmtp;
 
 use MassimoFilippi\MailModule\Adapter\AdapterInterface;
 use MassimoFilippi\MailModule\Model\Message\MessageInterface;
+use Zend\Mail\Message as ZendMessage;
+use Zend\Mail\Protocol\Smtp\Auth\Login as ZendProtocolAuthLogin;
+use Zend\Mail\Transport\Smtp as ZendTransport;
+use Zend\Mail\Transport\SmtpOptions as ZendTransportOptions;
 
 /**
  * Class SparkPostSmtpAdapter
@@ -11,12 +15,61 @@ use MassimoFilippi\MailModule\Model\Message\MessageInterface;
  */
 class SparkPostSmtpAdapter implements AdapterInterface
 {
+    const HOST = 'smtp.sparkpostmail.com';
+    const PORT = 587;
+
+    /**
+     * @var string
+     */
+    private $name = '';
+
+    /**
+     * @var string
+     */
+    private $password = '';
+
+    /**
+     * SparkPostSmtpAdapter constructor.
+     * @param string $name
+     * @param string $password
+     */
+    public function __construct($name, $password)
+    {
+        $this->name = (string)$name;
+        $this->password = (string)$password;
+    }
 
     /**
      * @param MessageInterface $message
      */
     public function sendMail(MessageInterface $message)
     {
-        // TODO: Implement sendMail() method.
+        $zendMessage = new ZendMessage();
+
+        $zendMessage->setEncoding('UTF-8');
+
+        $zendMessage->setFrom($message->getSender()->getEmail(), $message->getSender()->getName());
+
+        foreach ($message->getRecipients() as $recipient) {
+            $zendMessage->addTo($recipient->getEmail(), $recipient->getName());
+        }
+
+        $zendMessage->setSubject($message->getSubject());
+        $zendMessage->setBody($message->getMessage());
+
+        $options = new ZendTransportOptions([
+            'name' => $this->name,
+            'host' => self::HOST,
+            'port' => self::PORT,
+            'connection_class' => ZendProtocolAuthLogin::class,
+            'connection_config' => [
+                'username' => 'SMTP_Injection',
+                'password' => $this->password,
+                'ssl' => 'tls',
+            ],
+        ]);
+
+        $transport = new ZendTransport($options);
+        $transport->send($zendMessage);
     }
 }
