3b60d41dad611d8a6c9713f69c82c26d2a7c6585
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-10-23 13:41:33 +0200

    Update.

--- src/Adapter/SparkPost/SparkPostAdapter.php
+++ src/Adapter/SparkPost/SparkPostAdapter.php
@@ -2,8 +2,12 @@
 
 namespace MassimoFilippi\MailModule\Adapter\SparkPost;
 
+use GuzzleHttp\Client as GuzzleHttpClient;
+use Http\Adapter\Guzzle6\Client as GuzzleHttpAdapter;
 use MassimoFilippi\MailModule\Adapter\AdapterInterface;
 use MassimoFilippi\MailModule\Model\Message\MessageInterface;
+use SparkPost\SparkPost;
+use SparkPost\SparkPostPromise;
 
 /**
  * Class SparkPostAdapter
@@ -11,12 +15,74 @@ use MassimoFilippi\MailModule\Model\Message\MessageInterface;
  */
 class SparkPostAdapter implements AdapterInterface
 {
+    /**
+     * @var string
+     */
+    private $apiKey = '';
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * SparkPostAdapter constructor.
+     * @param array $options
+     */
+    public function __construct(array $options)
+    {
+        if (array_key_exists('api_key', $options)) {
+            $this->setApiKey($options['api_key']);
+        }
+    }
+
+    //-------------------------------------------------------------------------
 
     /**
      * @param MessageInterface $message
+     * @return SparkPostPromise
      */
     public function sendMail(MessageInterface $message)
     {
-        // TODO: Implement sendMail() method.
+
+        $httpClient = new GuzzleHttpAdapter(new GuzzleHttpClient());
+        $sparky = new SparkPost($httpClient, [
+            'key' => $this->getApiKey(),
+        ]);
+
+        $sparky->setOptions(['async' => false]);
+        $results = $sparky->transmissions->post([
+            'content' => [
+                'template_id' => 'recovery-email',
+            ],
+            'substitution_data' => [
+                'email' => $email,
+                'key' => $key,
+            ],
+            'recipients' => [
+                [
+                    'address' => [
+                        'email' => $email,
+                    ],
+                ],
+            ],
+        ]);
+
+        return $results;
+    }
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * @return string
+     */
+    public function getApiKey()
+    {
+        return $this->apiKey;
+    }
+
+    /**
+     * @param string $apiKey
+     */
+    public function setApiKey($apiKey)
+    {
+        $this->apiKey = (string)$apiKey;
     }
 }
