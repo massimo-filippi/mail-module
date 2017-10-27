d3f9318c449801e05aa51c762ff2edc93051a589
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-10-27 13:40:45 +0200

    Update

--- src/Model/Message/SparkPostMessage.php
+++ src/Model/Message/SparkPostMessage.php
@@ -9,4 +9,47 @@ namespace MassimoFilippi\MailModule\Model\Message;
 class SparkPostMessage extends Message implements MessageInterface
 {
 
+    /**
+     * @var bool
+     */
+    protected $template = false;
+
+    /**
+     * @var string
+     */
+    protected $templateId = '';
+
+    //-------------------------------------------------------------------------
+
+    /**
+     * @return bool
+     */
+    public function isTemplate()
+    {
+        return $this->template;
+    }
+
+    /**
+     * @param bool $template
+     */
+    public function setTemplate($template)
+    {
+        $this->template = (bool)$template;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTemplateId()
+    {
+        return $this->templateId;
+    }
+
+    /**
+     * @param string $templateId
+     */
+    public function setTemplateId($templateId)
+    {
+        $this->templateId = (string)$templateId;
+    }
 }
