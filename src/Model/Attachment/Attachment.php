cf3438556aa3a1dd7321e1f69d8c12fa2514f35c
Matus Petrofcik - behemoth <petrofcikmatus@gmail.com>
2017-10-27 14:23:47 +0200

    Prepared Attachment.

---
+++ src/Model/Attachment/Attachment.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace MassimoFilippi\MailModule\Model\Attachment;
+
+/**
+ * Class Attachment
+ * @package MassimoFilippi\MailModule\Model\Attachment
+ */
+class Attachment implements AttachmentInterface
+{
+
+    /**
+     * @var string
+     */
+    protected $name = '';
+
+    /**
+     * @var string
+     */
+    protected $type = '';
+
+    /**
+     * @var string
+     */
+    protected $data = '';
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+
+    /**
+     * @return string
+     */
+    public function getData()
+    {
+        return $this->data;
+    }
+
+    /**
+     * @param string $data
+     */
+    public function setData($data)
+    {
+        $this->data = $data;
+    }
+}
